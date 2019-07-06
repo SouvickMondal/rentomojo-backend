198fdadbd5a37442eb65258a37db317295893914
SouvickMondal <31573434+SouvickMondal@users.noreply.github.com>
2019-07-06 21:16:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ const app = express()
 app.use(cors())
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-// app.use((req,res,next)=>{
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// })
+app.use((req,res,next)=>{
+     res.header("Access-Control-Allow-Origin", "*");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     next();
+})
 async function registerUser(req,res){
     let email = req.body['email']
     let name = req.body['name']
